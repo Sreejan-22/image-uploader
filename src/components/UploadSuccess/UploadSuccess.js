0281c48896ee2573df59519d555c62df769be1fe
Sreejan <sreejan147@gmail.com>
2021-06-06 16:12:28 +0530

    implemented copy url to clipboard

--- src/components/UploadSuccess/UploadSuccess.js
+++ src/components/UploadSuccess/UploadSuccess.js
@@ -1,24 +1,40 @@
 import "./UploadSuccess.css";
 // import done from "../../assets/done.svg";
 import SuccessIcon from "./SuccessIcon";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const UploadSuccess = ({ imgUrl }) => {
-  useEffect(() => {
-    console.log(imgUrl);
-  }, [imgUrl]);
+  const [showAlert, setShowAlert] = useState(false);
   return (
     <div className="upload-success-box">
       <SuccessIcon />
       <div className="upload-success-text">Uploaded Successfully!</div>
-      {/* <div className="uploaded-img">
-        <div style={{height: "100%", width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-          <div style={{color: "#cccccc", fontWeight: "500"}}>Image Preview</div>
-        </div>
-      </div> */}
-      {/* https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80 */}
+      {/* https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&  ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80 */}
       <img src={imgUrl} alt="" className="uploaded-img" />
-      <div className="img-link-container"></div>
+      <form className="img-link-container">
+        <input
+          type="text"
+          readOnly
+          value={`  ${imgUrl}`}
+          className="url-text-input"
+        />
+        <button
+          className="copy-btn"
+          onClick={(e) => {
+            e.preventDefault();
+            setShowAlert(true);
+            setTimeout(() => {
+              setShowAlert(false);
+            }, 2000);
+            navigator.clipboard.writeText(imgUrl);
+          }}
+        >
+          Copy Link
+        </button>
+      </form>
+      <div style={{ display: showAlert ? "block" : "none", fontSize: "12px" }}>
+        Image URL Copied
+      </div>
     </div>
   );
 };
